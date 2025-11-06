4599af9385fed8561d5d6910824e0be562fa2fb3
Christopher Liu <liuchris@seas.upenn.edu>
2025-11-06 13:06:32 -0500

    Timer stuff

--- omnitiles/src/main.rs
+++ omnitiles/src/main.rs
@@ -8,8 +8,49 @@ use panic_halt as _;
 use hal::{pac, prelude::*};
 use stm32f7xx_hal as hal;
 
-#[allow(clippy::empty_loop)]
 #[entry]
 fn main() -> ! {
-    loop {}
+    // Peripherals
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Clocks
+    dp.RCC.apb1enr.modify(|_, w| w.tim5en().set_bit());
+
+    let rcc = dp.RCC.constrain();
+    let _clocks = rcc.cfgr.freeze();
+
+    // GPIO
+    let gpioa = dp.GPIOA.split();
+
+    // TIM5 (quadrature encoder)
+    let _tim5_ch1 = gpioa.pa0.into_alternate::<2>();
+    let _tim5_ch2 = gpioa.pa1.into_alternate::<2>();
+    let tim5 = dp.TIM5;
+    tim5.smcr.modify(|_, w| w.sms().bits(0b011));
+
+    tim5.ccmr1_input().modify(|_, w| {
+        w.cc1s().ti1();
+        w.cc2s().ti2();
+        w
+    });
+
+    tim5.ccer.modify(|_, w| {
+        w.cc1e()
+            .set_bit()
+            .cc1p()
+            .clear_bit()
+            .cc2e()
+            .set_bit()
+            .cc2p()
+            .clear_bit()
+    });
+
+    tim5.arr.write(|w| w.bits(0xFFFF_FFFF));
+    tim5.cnt.write(|w| w.bits(0));
+    tim5.cr1.modify(|_, w| w.cen().set_bit());
+
+    loop {
+        let _position: u32 = tim5.cnt.read().bits();
+        cortex_m::asm::nop();
+    }
 }
