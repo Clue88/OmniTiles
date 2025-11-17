024d8f843fe7eb5730c61b569a84b0b66caba01a
Christopher Liu <liuchris@seas.upenn.edu>
2025-11-17 15:28:46 -0500

    Docs and fix bug

--- omnitiles/src/main.rs
+++ omnitiles/src/main.rs
@@ -21,26 +21,26 @@ use hw::{CanBus, ChipSelect, Led, SpiBus, Usart};
 
 #[entry]
 fn main() -> ! {
-    // Peripherals
+    // ========== Peripherals ==========
     let dp = pac::Peripherals::take().unwrap();
 
-    // Clocks
+    // ========== Clocks ==========
     let rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.freeze();
     let mut apb1 = rcc.apb1;
     let mut apb2 = rcc.apb2;
 
-    // GPIO
+    // ========== GPIO ==========
     let gpioa = dp.GPIOA.split();
     let gpiob = dp.GPIOB.split();
     let gpiod = dp.GPIOD.split();
     let gpioe = dp.GPIOE.split();
 
-    // LED
+    // ========== LED ==========
     let mut led_yellow = Led::active_low(gpiod.pd9);
     let mut led_green = Led::active_low(gpiod.pd10);
 
-    // USART1 (DBG)
+    // ========== USART1 (DBG) ==========
     let usart_tx = gpioa.pa9.into_alternate::<7>();
     let usart_rx = gpioa.pa10.into_alternate::<7>();
     let usart_cfg = Config {
@@ -50,7 +50,7 @@ fn main() -> ! {
     let serial = Serial::new(dp.USART1, (usart_tx, usart_rx), &clocks, usart_cfg);
     let mut usart = Usart::new(serial);
 
-    // SPI4
+    // ========== SPI4 ==========
     let sck = gpioe.pe12.into_alternate::<5>();
     let miso = gpioe.pe13.into_alternate::<5>();
     let mosi = gpioe.pe14.into_alternate::<5>();
@@ -65,7 +65,7 @@ fn main() -> ! {
     let mut cs1 = ChipSelect::active_low(gpioe.pe4);
     let mut cs2 = ChipSelect::active_low(gpioe.pe11);
 
-    // CAN2
+    // ========== CAN2 ==========
     let pclk1_hz = clocks.pclk1().to_Hz();
     writeln!(usart, "PCLK1 = {} Hz\r", pclk1_hz).ok(); // TODO: Use this to calculate CAN_BTR
     const CAN_BTR: u32 = 0x001c_0014; // TODO: Recalculate based on actual APB1 frequency
@@ -77,12 +77,12 @@ fn main() -> ! {
     can1_bus.configure_accept_all_filters(); // Configure filters on CAN1
     drop(can1_bus);
 
-    let can2_tx = gpiob.pb12.into_alternate::<9>();
-    let can2_rx = gpiob.pb13.into_alternate::<9>();
-    let can2_hal = Can::new(dp.CAN2, &mut apb1, (can2_rx, can2_tx));
-    let mut can_bus = CanBus::new(can2_hal, CAN_BTR, true, false);
+    let can2_tx = gpiob.pb13.into_alternate::<9>();
+    let can2_rx = gpiob.pb12.into_alternate::<9>();
+    let can2_hal = Can::new(dp.CAN2, &mut apb1, (can2_tx, can2_rx));
+    let mut can_bus = CanBus::new(can2_hal, CAN_BTR, true, false); // Loopback mode
 
-    // EXAMPLE USAGES
+    // ========== EXAMPLE USAGES ==========
     led_yellow.on();
     led_green.on();
 
@@ -95,8 +95,8 @@ fn main() -> ! {
     let _ = spi_bus.transfer_byte(0xFE);
     cs2.deselect();
 
-    let id = StandardId::new(0x123).unwrap();
-    let _ = can_bus.transmit_data(id, &[0xDE, 0xAD, 0xBE, 0xEF]);
+    let can_id = StandardId::new(0x123).unwrap();
+    let _ = can_bus.transmit_data(can_id, &[0xDE, 0xAD, 0xBE, 0xEF]);
 
     loop {
         cortex_m::asm::nop();
